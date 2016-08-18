66d6fbfc09052854801fcc9a6c676f25f103d82c
Jeff Crone <github@314ghz.com>
2016-08-18 08:46:08 -0400

    Initial build of the Gradle project.

---
+++ src/test/java/net/codehobby/fileinfo/FileInfoTest.java
@@ -0,0 +1,14 @@
+package net.codehobby.fileinfo;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class FileInfoTest
+{
+/*
+    @Test public void testSomeLibraryMethod() {
+        Library classUnderTest = new Library();
+        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+    }
+*/
+}
